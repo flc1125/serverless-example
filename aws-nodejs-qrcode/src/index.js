262f40f1bded735ebd130c0003097101c0704651
flc1125 <four_leaf_clover@foxmail.com>
2020-05-22 01:02:51 +0800

    init

---
+++ aws-nodejs-qrcode/src/index.js
@@ -0,0 +1,24 @@
+const qrcode = require('qrcode');
+
+exports.qrcode = async(event, context, callback) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(event)
+    };
+
+    let { text, margin, width } = event.queryString;
+
+    let options = {
+        margin: margin || 1,
+        width: width || 250
+    }
+
+    result = await qrcode.toDataURL(text, options)
+
+    return {
+        "isBase64Encoded": true,
+        "statusCode": 200,
+        "headers": { "Content-Type": "image/png" },
+        "body": result.replace(new RegExp('data:image/png;base64,'), '')
+    };
+}
